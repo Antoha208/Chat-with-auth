d0457be22d1fb7df5d4152c5b2273594bf32971c
Anton Androsiuk <67116318+Antoha208@users.noreply.github.com>
2022-03-25 12:32:43 +0300

    Routes
    authRoutes

---
+++ authController.js
@@ -0,0 +1,27 @@
+class authController {
+    async registration(req, res) {
+        try {
+            
+        } catch (error) {
+            
+        }
+    }
+
+    async login(req, res) {
+        try {
+            
+        } catch (error) {
+            
+        }
+    }
+
+    async getUsers(req, res) {
+        try {
+            res.json('server is working')
+        } catch (error) {
+            console.log(error)
+        }
+    }
+}
+
+export default new authController()

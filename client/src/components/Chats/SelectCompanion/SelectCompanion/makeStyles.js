0925f47eded552d7eb961410da3b01b86b18a5f5
Anton Androsiuk <67116318+Antoha208@users.noreply.github.com>
2023-01-27 02:15:07 +0300

    Update makeStyles SelectCompanion
    Update 2 blocks of imports with files extensions and custom hook instead of makeStyles and import UI

--- client/src/components/Chats/SelectCompanion/SelectCompanion/makeStyles.js
+++ client/src/components/Chats/SelectCompanion/SelectCompanion/makeStyles.js
@@ -1,36 +1,34 @@
-import { makeStyles } from '@material-ui/core/styles';
+const stylesJS = (theme) => ({
+  root: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'flex-start',
+    alignItems: 'flex-start',
+    width: '100%',
+    backgroundColor: theme.palette.background.paper,
+    padding: '2px 0px',
+    overflowX: 'hidden'
+  },
 
-const useStyles = makeStyles((theme) => ({
-    root: {
-      display: 'flex',
-      flexDirection: 'column',
-      justifyContent: 'flex-start',
-      alignItems: 'flex-start',
-      width: '100%',
-      backgroundColor: theme.palette.background.paper,
-      padding: '2px 0px',
-      overflowX: 'hidden'
-    },
+  inputWrap: {
+    display: 'flex',
+    width: '97%',
+    backgroundColor: '#1A1B1A',
+    margin: '2px 5px',
+  },
 
-    inputWrap: {
-      display: 'flex',
-      width: '97%',
-      backgroundColor: '#1A1B1A',
-      margin: '2px 5px',
-    },
+  input: {
+    marginLeft: theme.spacing(1),
+    flex: 1,
+  },
+  
+  button: {
+    width: '100%',
+  },
 
-    input: {
-      marginLeft: theme.spacing(1),
-      flex: 1,
-    },
-    
-    button: {
-      width: '100%',
-    },
+  iconButton: {
+    padding: 10,
+  }
+})
 
-    iconButton: {
-      padding: 10,
-    }
-  }))
-
-  export default useStyles
+export default stylesJS

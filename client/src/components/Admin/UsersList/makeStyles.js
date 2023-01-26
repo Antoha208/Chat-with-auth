f5f7e4604a64637f462dd7202f0aaa01a1c55174
Anton Androsiuk <67116318+Antoha208@users.noreply.github.com>
2023-01-27 01:25:29 +0300

    Update makeStyles UsersList
    Update 2 blocks of imports with files extensions and custom hook instead of makeStyles and import UI

--- client/src/components/Admin/UsersList/makeStyles.js
+++ client/src/components/Admin/UsersList/makeStyles.js
@@ -1,42 +1,40 @@
-import { makeStyles } from '@material-ui/core/styles';
+const stylesJS = theme => ({
+  root: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'flex-start',
+    alignItems: 'flex-start',
+    width: '100%',
+    maxWidth: 400,
+    maxHeight: 400,
+    backgroundColor: theme.palette.background.paper,
+    overflow: 'auto',
+    padding: '2px 0px',
+  },
 
-const useStyles = makeStyles((theme) => ({
-    root: {
-      display: 'flex',
-      flexDirection: 'column',
-      justifyContent: 'flex-start',
-      alignItems: 'flex-start',
-      width: '100%',
-      maxWidth: 400,
-      maxHeight: 400,
-      backgroundColor: theme.palette.background.paper,
-      overflow: 'auto',
-      padding: '2px 0px',
-    },
+  inputWrap: {
+    display: 'flex',
+    width: '97%',
+    backgroundColor: '#1A1B1A',
+    margin: '2px 5px',
+  },
 
-    inputWrap: {
-      display: 'flex',
-      width: '97%',
-      backgroundColor: '#1A1B1A',
-      margin: '2px 5px',
-    },
+  input: {
+    marginLeft: theme.spacing(1),
+    flex: 1,
+  },
 
-    input: {
-      marginLeft: theme.spacing(1),
-      flex: 1,
-    },
+  iconButton: {
+    padding: 10,
+  },
+  
+  paperButton: {
+    width: '100%',
+    display: 'flex',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    minHeight: '56px',
+  },
+})
 
-    iconButton: {
-      padding: 10,
-    },
-    
-    paperButton: {
-      width: '100%',
-      display: 'flex',
-      justifyContent: 'space-around',
-      alignItems: 'center',
-      minHeight: '56px',
-    },
-  }));
-
-  export default useStyles
+export default stylesJS

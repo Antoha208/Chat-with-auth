dbb3c64173756b4b70fd4b20ae7d0388738a0eb2
Anton Androsiuk <67116318+Antoha208@users.noreply.github.com>
2023-01-27 02:24:54 +0300

    Update ProfilePhoto
    Update 2 blocks of imports with files extensions and custom hook instead of makeStyles and import UI

--- client/src/components/Profile/ProfilePhoto/ProfilePhoto.js
+++ client/src/components/Profile/ProfilePhoto/ProfilePhoto.js
@@ -2,23 +2,26 @@ import React, { useState } from 'react'
 import { useTranslation } from 'react-i18next'
 import { useDispatch, useSelector } from 'react-redux'
 
-import Avatar from '@material-ui/core/Avatar'
-import IconButton from '@material-ui/core/IconButton'
-import Tooltip from '@material-ui/core/Tooltip'
-import PhotoCamera from '@material-ui/icons/PhotoCamera'
-import DeleteForeverRoundedIcon from '@material-ui/icons/DeleteForeverRounded'
-import Paper from '@material-ui/core/Paper'
+
+import Avatar from '@mui/material/Avatar'
+import IconButton from '@mui/material/IconButton'
+import Tooltip from '@mui/material/Tooltip'
+import Paper from '@mui/material/Paper'
+import PhotoCameraRoundedIcon from '@mui/icons-material/PhotoCameraRounded'
+import DeleteForeverRoundedIcon from '@mui/icons-material/DeleteForeverRounded'
+
 
 import StyledBadge from './withStyles.js'
-import useStyles from './makeStyles.js'
+import stylesJS from './makeStyles.js'
+import useClasses from '../../../CustomHooks/useClasses.js'
 import styles from './ProfilePhoto.module.css'
-import { uploadAvatar, deleteAvatar } from '../../../http/fileApi'
-import { resetProfilePhoto, setProfilePhoto } from '../../../store/userReducer'
+import { uploadAvatar, deleteAvatar } from '../../../http/fileApi.js'
+import { resetProfilePhoto, setProfilePhoto } from '../../../store/userReducer.js'
 
 
 const ProfilePhoto = () => {
   const userStore = useSelector(state => state.user.user)
-  const classes = useStyles()
+  const classes = useClasses(stylesJS)
   const { t } = useTranslation()
   const [avatar, setAvatar] = useState(userStore.avatar)
   const dispatch = useDispatch()
@@ -46,10 +49,7 @@ const ProfilePhoto = () => {
       <Paper className={classes.avatarContainer}>
         <StyledBadge
           overlap="circular"
-          anchorOrigin={{
-            vertical: 'bottom',
-            horizontal: 'right',
-          }}
+          anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
           variant="dot"
         >
           {userStore.avatar === '' || undefined || null ? 
@@ -71,7 +71,7 @@ const ProfilePhoto = () => {
           <label htmlFor="icon-button-file">
             <IconButton color="primary" aria-label="upload picture" component="span">
               <Tooltip title={t ('description.ProfileUploadTooltip')} arrow>
-                <PhotoCamera className={styles.icon} />
+                <PhotoCameraRoundedIcon className={styles.icon} />
               </Tooltip>
             </IconButton>
           </label>

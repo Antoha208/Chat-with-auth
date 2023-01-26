e09faefb0540a9b93fff9f27df97bdbf385283a4
Anton Androsiuk <67116318+Antoha208@users.noreply.github.com>
2023-01-27 01:28:37 +0300

    Update AppRouter
    Update 2 blocks of imports with files extensions and custom hook instead of makeStyles and import UI

--- client/src/components/App/AppRouter.js
+++ client/src/components/App/AppRouter.js
@@ -2,8 +2,9 @@ import React from 'react'
 import { Routes, Route, Navigate } from 'react-router-dom'
 import { useSelector } from 'react-redux'
 
-import { authRoutes, allRoutes } from './routes'
-import {REGISTRATION_ROUTE} from '../../utils/consts'
+
+import { authRoutes, allRoutes } from './routes.js'
+import {REGISTRATION_ROUTE} from '../../utils/consts.js'
 
 const AppRouter = () => {
   const isAuth = useSelector(state => state.isAuth.isAuth)

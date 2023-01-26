4a649e460f7574bd53da88f0d3a97e67e847d12e
Anton Androsiuk <67116318+Antoha208@users.noreply.github.com>
2023-01-27 01:53:28 +0300

    Update makeStyles BottomBar
    Update 2 blocks of imports with files extensions and custom hook instead of makeStyles and import UI

--- client/src/components/Chats/ChatWindow/BottomBar/makeStyles.js
+++ client/src/components/Chats/ChatWindow/BottomBar/makeStyles.js
@@ -1,29 +1,26 @@
-import { makeStyles } from '@material-ui/core/styles';
+const stylesJS = theme => ({
+  root: {
+    padding: '2px 4px',
+    display: 'flex',
+    alignItems: 'center',
+    width: '95%',
+    margin: '15px 0px 10px 7px',
+  },
 
-const useStyles = makeStyles((theme) => ({
-    root: {
-      padding: '2px 4px',
-      display: 'flex',
-      alignItems: 'center',
-      width: '95%',
-      margin: '15px 0px 10px 7px',
-    },
+  input: {
+    display: 'none',
+    marginLeft: theme.spacing(1),
+    flex: 1,
+  },
 
-    input: {
-      display: 'none',
-      marginLeft: theme.spacing(1),
-      flex: 1,
-    },
+  iconButton: {
+    padding: 10,
+  },
 
-    iconButton: {
-      padding: 10,
-    },
+  divider: {
+    height: 40,
+    margin: 4,
+  },
+})
 
-    divider: {
-      height: 40,
-      margin: 4,
-    },
-    
-  }));
-
-  export default useStyles
+export default stylesJS

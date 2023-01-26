8d44e07312a42ec965f8a035eb225fd1035b6cea
Anton Androsiuk <67116318+Antoha208@users.noreply.github.com>
2023-01-27 02:18:22 +0300

    Update makeStyles AvatarCompanion
    Update 2 blocks of imports with files extensions and custom hook instead of makeStyles and import UI

--- client/src/components/NavBar/AvatarComponent/makeStyles.js
+++ client/src/components/NavBar/AvatarComponent/makeStyles.js
@@ -1,10 +1,8 @@
-import { makeStyles } from '@material-ui/core/styles';
+const stylesJS = theme => ({
+  small: {
+    width: theme.spacing(5),
+    height: theme.spacing(5),
+  },
+})
 
-const useStyles = makeStyles((theme) => ({
-    small: {
-      width: theme.spacing(5),
-      height: theme.spacing(5),
-    },
-  }));
-
-export default useStyles
+export default stylesJS

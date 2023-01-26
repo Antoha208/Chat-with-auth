dffbde9d6b697402cb54ea39117fea60614143d2
Anton Androsiuk <67116318+Antoha208@users.noreply.github.com>
2023-01-27 01:24:13 +0300

    Update UsersList
    Update 2 blocks of imports with files extensions and custom hook instead of makeStyles and import UI

--- client/src/components/Admin/UsersList/UsersList.js
+++ client/src/components/Admin/UsersList/UsersList.js
@@ -4,25 +4,26 @@ import { useNavigate } from 'react-router-dom'
 import { useSelector } from 'react-redux'
 
 
-import List from '@material-ui/core/List'
-import Button from '@material-ui/core/Button'
-import Paper from '@material-ui/core/Paper'
-import InputBase from '@material-ui/core/InputBase'
-import SearchIcon from '@material-ui/icons/Search'
-import IconButton from '@material-ui/core/IconButton'
+import List from '@mui/material/List'
+import Button from '@mui/material/Button'
+import Paper from '@mui/material/Paper'
+import InputBase from '@mui/material/InputBase'
+import IconButton from '@mui/material/IconButton'
+import SearchRoundedIcon from '@mui/icons-material/SearchRounded'
 
 
-import useStyles from './makeStyles'
+import stylesJS from './makeStyles.js'
+import useClasses from '../../../CustomHooks/useClasses.js'
 import styles from './UsersList.module.css'
-import localReducer from './localReducer'
-import UserComp from '../UserComp/UserComp'
-import { REGISTRATION_ROUTE } from '../../../utils/consts'
-import { deleteAllUsers, deleteOneUser } from '../../../http/userApi'
+import localReducer from './localReducer.js'
+import UserComp from '../UserComp/UserComp.js'
+import { REGISTRATION_ROUTE } from '../../../utils/consts.js'
+import { deleteAllUsers, deleteOneUser } from '../../../http/userApi.js'
 
 
 const UsersList = () => {
   const { t } = useTranslation()
-  const classes = useStyles()
+  const classes = useClasses(stylesJS)
   const userStore = useSelector(state => state.user.user)
   const allUsers = useSelector(state => state.users.users)
   const [states, localDispatch] = useReducer(localReducer, {checked: [1], count: '', search: ''})
@@ -57,7 +58,7 @@ const UsersList = () => {
           onChange={(e) => localDispatch({type: 'search', payload: e.target.value})}
         />
         <IconButton disabled className={classes.iconButton}>
-          <SearchIcon className = {styles.icon} />
+          <SearchRoundedIcon className = {styles.icon} />
         </IconButton>
       </Paper>
         {filterUsers.map((user, index) => {

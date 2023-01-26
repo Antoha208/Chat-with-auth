13d07af08fe9986b5209180a3bc50839e7f640fd
Anton Androsiuk <67116318+Antoha208@users.noreply.github.com>
2023-01-27 02:35:39 +0300

    Update makeStyles SimpleModal
    Update 2 blocks of imports with files extensions and custom hook instead of makeStyles and import UI

--- client/src/components/Settings/Modal/SimpleModal/makeStyles.js
+++ client/src/components/Settings/Modal/SimpleModal/makeStyles.js
@@ -1,17 +1,15 @@
-import { makeStyles } from '@material-ui/core/styles';
+const stylesJS = theme => ({
+  cardContent: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    flexDirection: 'column',
+    height: '55%'
+  },
 
-const useStyles = makeStyles((theme) => ({
-    cardContent: {
-      display: 'flex',
-      justifyContent: 'space-between',
-      alignItems: 'center',
-      flexDirection: 'column',
-      height: '55%'
-    },
+  button: {
+    fontSize: '1.1rem'
+  }
+})
 
-    button: {
-      fontSize: '1.1rem'
-    }
-  }));
-
-  export default useStyles
+export default stylesJS
